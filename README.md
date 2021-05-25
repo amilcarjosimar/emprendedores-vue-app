9ad2a112caf8bd0097e5bc967e25396e98cf0bbf
Maquina Virtual-19-2384 <FernandoJoseOrdonezRamirez@tarea.com>
2021-05-24 22:24:00 -0600

    adding fully functional navbar

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 
 ## Archivos de app vue para proyecto emprendedores
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,117 @@
 <template>
-  <nav class="nav">
+  <div class="nav-header" id="nav-header-container">
+    <i class="fas fa-box-open nav-header-icon"></i>
+    <p class="nav-header-text">Te lo traigo</p>
+     <div id="fa-toggle-container" @click="toggle_menu">
+      <i class="fas fa-bars nav-tabs" id="nav-header-toggle" ></i>
+     </div>
+  </div>
+  <nav class="nav" id="dropdown">
     <div class="nav-item">
       <i class="fas fa-home nav-item-icon"></i>
-      <p class="nav-item-text"></p>
+      <p class="nav-item-text">Inicio</p>
+    </div>
+    <div class="nav-item">
+      <i class="fas fa-search-dollar nav-item-icon"></i>
+      <p class="nav-item-text">Ofertas</p>
+    </div>
+    <div class="nav-item">
+      <i class="fas fa-comments-dollar nav-item-icon"></i>
+      <p class="nav-item-text">Comprar</p>
+    </div>
+    <div class="nav-item">
+      <i class="fas fa-comments-dollar nav-item-icon"></i>
+      <p class="nav-item-text">Vender</p>
     </div>
   </nav>
   <router-view />
 </template>
-
-<style>
-  .nav{
-    background: black;
-    color: white;
+<script>
+export default {
+  methods:{
+    toggle_menu(){
+      let target = document.querySelector("#nav-header-toggle");
+      let dropdown = document.querySelector("#dropdown");
+      let container = document.querySelector("#fa-toggle-container");
+      let nav_header = document.querySelector("#nav-header-container");
+      nav_header.classList.toggle("nav-header-dormant")
+      target.classList.toggle("fa-bars")
+      target.classList.toggle("fa-times")
+      container.classList.toggle("active-times")
+      dropdown.classList.toggle("active")
+    }
   }
+}
+</script>
+<style scoped>
+.nav-header-text {
+  width: 100%;
+  font-family: "Permanent Marker", cursive;
+  font-size: 1.5rem;
+}
+.nav-header {
+    position: relative;
+  display: flex;
+   background-color: var(--black);
+  justify-content: space-around;
+  padding: 1rem 0;
+  font-size: 2rem;
+  box-sizing: border-box;
+  text-align: center;
+  color: var(--amber);
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px 0px;
+   text-shadow: 2px 1px var(--orange);
+}
+.nav-header > [class*="fa-"] {
+  margin: auto;
+}
+.nav-header {
+  width: 100%;
+  max-height: 100%;
+}
+.nav-header-dormant{
+  height: 0;
+}
+.nav-header-dormant > .nav-header-icon ,.nav-header-dormant > .nav-header-text{
+  display: none;
+}
+.active-times{
+  position: absolute;
+  z-index: 999999;
+  font-size: 3rem;
+  left: 80%;
+}
+.active {
+  height: 100vh !important;
+}
+.nav {
+  background-color: var(--black);
+  position: absolute;
+  color: var(--amber);
+  height: 0vh;
+  width: 100%;
+  top: 0;
+  z-index: 99999;
+  font-size: 3rem;
+  font-family: "Farro", sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  transition: height .40s ease-in-out;
+  overflow: hidden;
+  text-shadow: 2px 1px var(--orange);
+}
+[class*="fa-"] {
+  display: inline-block;
+  width: 100%;
+}
+.nav-item {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  padding: 0.5rem;
+}
+.nav-item-icon {
+  margin-right: 2rem;
+}
 </style>
